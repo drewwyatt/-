f18abf0aff000a4229d0dbccbf970bbc19200467
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-12-08 01:22:49 -0500

    day 3, part 1

--- src/bin/day-3/main.rs
+++ src/bin/day-3/main.rs
@@ -1,11 +1,103 @@
 use advent::read_input_for_day;
 use std::io;
 
-fn part_one(input: &Vec<String>) -> (usize, usize) {
-  let mut gamma = 0;
-  let mut epsilon = 0;
+enum AdventError {
+  InvalidBit,
+}
 
-  (gamma, epsilon)
+struct Position {
+  zeroes: usize,
+  ones: usize,
+}
+
+impl Position {
+  pub fn new() -> Position {
+    Position { zeroes: 0, ones: 0 }
+  }
+
+  pub fn push(&mut self, bit: &char) -> Result<(), AdventError> {
+    match bit {
+      '0' => {
+        self.zeroes += 1;
+        Ok(())
+      }
+      '1' => {
+        self.ones += 1;
+        Ok(())
+      }
+      _ => Err(AdventError::InvalidBit),
+    }
+  }
+
+  pub fn get_gamma(&self) -> &str {
+    if self.zeroes > self.ones {
+      "0"
+    } else {
+      "1"
+    }
+  }
+
+  pub fn get_epsilon(&self) -> &str {
+    if self.zeroes > self.ones {
+      "1"
+    } else {
+      "0"
+    }
+  }
+}
+
+struct Diagnostic {
+  positions: Vec<Position>,
+}
+
+impl Diagnostic {
+  pub fn new() -> Diagnostic {
+    Diagnostic { positions: vec![] }
+  }
+
+  pub fn get_gamma(&self) -> String {
+    let mut gamma = "".to_owned();
+    for position in self.positions.iter() {
+      gamma.push_str(position.get_gamma());
+    }
+
+    gamma
+  }
+
+  pub fn get_epsilon(&self) -> String {
+    let mut epsilon = "".to_owned();
+    for position in self.positions.iter() {
+      epsilon.push_str(position.get_epsilon());
+    }
+
+    epsilon
+  }
+
+  pub fn push(&mut self, bit_index: usize, value: &char) -> Result<(), AdventError> {
+    match self.positions.get_mut(bit_index) {
+      Some(position) => position.push(value),
+      None => {
+        let mut position = Position::new();
+        let res = position.push(value);
+        self.positions.push(position);
+        res
+      }
+    }
+  }
+}
+
+fn part_one(input: &Vec<String>) -> (i64, i64) {
+  let mut diagnostic = Diagnostic::new();
+  for line in input {
+    for (bit_index, value) in line.chars().enumerate() {
+      diagnostic.push(bit_index, &value);
+    }
+  }
+
+  (
+    i64::from_str_radix(&diagnostic.get_gamma(), 2).unwrap(),
+    i64::from_str_radix(&diagnostic.get_epsilon(), 2).unwrap(),
+  )
 }
 
 #[cfg(test)]
