b619f50ff364b782e5aa4b75c965250b627fd4b5
Drew Wyatt <drew.j.wyatt@gmail.com>
2021-12-16 22:05:10 -0500

    can recognize winning rows

--- src/bin/day-4/models.rs
+++ src/bin/day-4/models.rs
@@ -17,7 +17,7 @@ impl Board {
     }
   }
 
-  pub fn has_winner(&self) -> bool {
+  pub fn is_winner(&self) -> bool {
     self
       .rows()
       .iter()
@@ -28,7 +28,7 @@ impl Board {
     let mut result: Vec<&[Space]> = vec![];
     for i in 0..4 {
       let start = i * 5;
-      let end = start + 4;
+      let end = start + 5;
       result.push(&self.spaces[start..end]);
     }
 
@@ -57,17 +57,57 @@ impl Space {
 #[cfg(test)]
 mod tests {
   use super::*;
-  fn get_boards() -> Vec<Board> {
-    vec![
-      Board::new(vec![
-        22, 13, 17, 11, 0, 8, 2, 23, 4, 24, 21, 9, 14, 16, 7, 6, 10, 3, 18, 5, 1, 12, 20, 15, 19,
-      ]),
-      Board::new(vec![
-        3, 15, 0, 2, 22, 9, 18, 13, 17, 5, 19, 8, 7, 25, 23, 20, 11, 10, 24, 4, 14, 21, 16, 12, 6,
-      ]),
-      Board::new(vec![
-        14, 21, 17, 24, 4, 10, 16, 15, 9, 19, 18, 8, 23, 26, 20, 22, 11, 13, 6, 5, 2, 0, 12, 3, 7,
-      ]),
-    ]
+  fn make_board_1() -> Board {
+    Board::new(vec![
+      22, 13, 17, 11, 0, 8, 2, 23, 4, 24, 21, 9, 14, 16, 7, 6, 10, 3, 18, 5, 1, 12, 20, 15, 19,
+    ])
+  }
+
+  fn make_board_2() -> Board {
+    Board::new(vec![
+      3, 15, 0, 2, 22, 9, 18, 13, 17, 5, 19, 8, 7, 25, 23, 20, 11, 10, 24, 4, 14, 21, 16, 12, 6,
+    ])
+  }
+
+  fn make_board_3() -> Board {
+    Board::new(vec![
+      14, 21, 17, 24, 4, 10, 16, 15, 9, 19, 18, 8, 23, 26, 20, 22, 11, 13, 6, 5, 2, 0, 12, 3, 7,
+    ])
+  }
+
+  fn make_boards() -> Vec<Board> {
+    vec![make_board_1(), make_board_2(), make_board_3()]
+  }
+
+  #[test]
+  fn non_winners() {
+    let mut board = make_board_1();
+    assert_eq!(board.is_winner(), false);
+
+    board.check(22);
+    board.check(13);
+    board.check(17);
+    board.check(11);
+    assert_eq!(board.is_winner(), false);
+  }
+
+  #[test]
+  fn can_recognize_winning_row() {
+    let mut boards = make_boards();
+    let spaces = vec![
+      vec![22, 13, 17, 11, 0],
+      vec![9, 18, 13, 17, 5],
+      vec![18, 8, 23, 26, 20],
+    ];
+
+    for (index, group) in spaces.iter().enumerate() {
+      for space in group {
+        boards[index].check(*space);
+      }
+    }
+
+    for board in boards {
+      assert_eq!(board.is_winner(), true);
+    }
   }
 }
