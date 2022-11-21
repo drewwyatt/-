a554fc56b7942f84ba922fbc39e4eceef2a4465c
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-11-20 19:47:28 -0500

    wip: charting coords

--- src/bin/day-5/input.rs
+++ src/bin/day-5/input.rs
@@ -1,15 +1,27 @@
 use advent::parse_from_named_captures_or;
 use regex::Regex;
-use std::str::FromStr;
+use std::{str::FromStr, collections::HashMap};
 
 #[derive(Clone, Copy, Debug)]
 pub enum AdventError {
   InvalidRegex,
 }
 
+type Coord = (i32, i32);
+
 pub struct Line {
-  pub start: (i32, i32),
-  pub end: (i32, i32),
+  pub start: Coord,
+  pub end: Coord,
+}
+
+impl Line {
+  pub fn is_horizontal(&self) -> bool {
+    self.start.0 == self.end.0
+  }
+
+  pub fn is_vertical(&self) -> bool {
+    !&self.is_horizontal()
+  }
 }
 
 impl FromStr for Line {
@@ -29,5 +41,29 @@ impl FromStr for Line {
     let y2 = parse_from_named_captures_or(&captures, "y2", AdventError::InvalidRegex)?;
 
     Ok(Line { start: (x1, y1), end: (x2, y2) })
+  }
+}
+
+pub struct Graph {
+  values: HashMap<Coord, i32>
+}
+
+impl Graph {
+  pub fn chart(&self, line: Line) {
+    if line.is_horizontal() {
+      let x = line.start.0;
+      for y in line.start.1..line.end.1 {
+        self.mark_coordinate(x, y);
+      }
+    } else {
+      let y = line.start.1;
+      for x in line.start.0..line.end.0 {
+        self.mark_coordinate(x, y);
+      }
+    }
+  }
+
+  fn mark_coordinate(&self, x: i32, y: i32) {
+
   }
 }
