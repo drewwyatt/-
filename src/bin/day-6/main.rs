71c4bf8b855c468401327fed8bdb73ebb7ef94c4
Drew Wyatt <drew.j.wyatt@gmail.com>
2022-11-21 21:43:23 -0500

    pass part_one test

--- src/bin/day-6/main.rs
+++ src/bin/day-6/main.rs
@@ -1,6 +1,31 @@
 mod models;
 use std::io;
 
+use models::school::School;
+
 fn main() -> io::Result<()> {
   Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use std::str::FromStr;
+
+  fn get_initial_state() -> School {
+    School::from_str("3,4,3,1,2").unwrap()
+  }
+
+  #[test]
+  fn part_one() {
+    let mut school = get_initial_state();
+    for _ in 0..80 {
+      school.tick();
+    }
+
+    assert_eq!(school.size(), 5934);
+  }
+
+  #[test]
+  fn part_two() {}
 }
--- src/bin/day-6/models/school.rs
+++ src/bin/day-6/models/school.rs
@@ -1,12 +1,31 @@
 use super::fish::Fish;
 use std::str::FromStr;
 
-enum AdventError {
-  InvalidInput,
+#[derive(Debug)]
+pub enum AdventError {}
+
+pub struct School {
+  fish: Vec<Fish>,
 }
 
-struct School {
-  fish: Vec<Fish>,
+impl School {
+  pub fn size(&self) -> usize {
+    self.fish.len()
+  }
+
+  pub fn tick(&mut self) {
+    let mut babies = vec![];
+    for next_fish in self.fish.iter_mut() {
+      match next_fish.tick() {
+        Some(baby) => babies.push(baby),
+        _ => (),
+      }
+    }
+
+    if !babies.is_empty() {
+      self.fish.append(&mut babies);
+    }
+  }
 }
 
 impl FromStr for School {
